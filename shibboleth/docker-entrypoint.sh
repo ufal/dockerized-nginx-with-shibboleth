29b0f00b6c8e6909588ddc26986089202a05cb0b
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2024-01-05 17:46:20 +0100

    handling of sp-keys

--- shibboleth/docker-entrypoint.sh
+++ shibboleth/docker-entrypoint.sh
@@ -5,6 +5,14 @@ set -e
 # envsubst is in gettext-base
 # xmllint is in libxml2-utils
 
+# use keys in /sp-keys if they exist, otherwise generate them
+cd /sp-keys
+/opt/shibboleth-sp/etc/shibboleth/keygen.sh -n sp-signing || true
+/opt/shibboleth-sp/etc/shibboleth/keygen.sh -n sp-encrypt || true
+
+# copy keys to /opt/shibboleth-sp/etc/shibboleth
+cp -Rv /sp-keys/. /opt/shibboleth-sp/etc/shibboleth/
+
 # if dir /overrides exists, overlay it on top of /opt/shibboleth-sp
 if [ -d /overrides ]; then
     echo "Overlaying /overrides on top of /opt/shibboleth-sp"
