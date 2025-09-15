56ead273f3574b47d32c1a88838c32a5845d3af9
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2025-09-15 07:44:07 +0200

    Update shibboleth-sp version to 3.5.1

--- shibboleth/shibboleth_sp_with_fastcgi.sh
+++ shibboleth/shibboleth_sp_with_fastcgi.sh
@@ -26,7 +26,7 @@ get xerces-c 3.3.0 http://mirror.hosting90.cz/apache/xerces/c/3/sources/
 get xml-security-c 3.0.0 https://shibboleth.net/downloads/xml-security-c/3.0.0/
 get xmltooling 3.3.0 http://shibboleth.net/downloads/c++-opensaml/latest/
 get opensaml 3.3.1 http://shibboleth.net/downloads/c++-opensaml/latest/
-get shibboleth-sp 3.5.0 http://shibboleth.net/downloads/service-provider/latest/
+get shibboleth-sp 3.5.1 http://shibboleth.net/downloads/service-provider/latest/
 
 
 function compile {
